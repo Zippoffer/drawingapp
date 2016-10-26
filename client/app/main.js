69ead92e35a68d3750cdfafb81448fd680608777
Christopher Hill <thorsdaddy77@gmail.com>
2016-10-26 14:05:29 -0500

    images are displaying, and deleting

--- client/app/main.js
+++ client/app/main.js
@@ -13,9 +13,11 @@ angular
 				controller: 'DrawCtrl',
 				templateUrl: 'partials/canvi.html'
 			})
+			.when('/pic', {
+				controller: 'ShowCtrl',
+				templateUrl: 'partials/pic.html'
+			})
 		)
-
-
 
 	.controller('MainCtrl', function ($scope, $http) {
 		$http
@@ -25,47 +27,66 @@ angular
 			})
 	})
 
+
+//////////showCtrl\\\\\\\\\\\\\
+ .controller('ShowCtrl', function($scope, $http, $routeParams){
+ 	$scope.seePics = (canviId, index) => {
+ 		
+ 		$http
+			.get('/api/pics')
+			.then(({data: {pics}}) => {
+				$scope.pics = pics
+				console.log('pics', pics)
+			})
+ 		}
+ 		$scope.seePics()
+ 
+ 		$scope.remove = (picId, index) => { 
+				$http
+				.post('/api/remove', {picId})
+				.then(() => $scope.pics.splice(index, 1))
+				console.log('picId',picId)    
+			}
+ })
+///////////showCtrl\\\\\\\\\\\\\\\
+
+
 // drawingboard stuff \\
 													
   .controller('DrawCtrl', function ($scope, $http) {
 
-			console.log('canvas', canvas)
+			// console.log('canvas', canvas)
 			let board = new DrawingBoard.Board('canvas',{
-					background: "#ffffff",
+				background: "#FAE5D3",
 		    color: "#000000",
 		    size: 1,
-		controls: [
-	        { Size: { type: "range" } },
-	        { Navigation: { back: true, forward: true } },
-	        'DrawingMode',
-	        'Color',
-	        // 'Download',
-
-    ],
-    webStorage: 'local'
+				controls: [
+			        { Size: { type: "range" } },
+			        { Navigation: { back: true, forward: true } },
+			        'DrawingMode',
+			        'Color',
+		    ],
+		    webStorage: 'local'
 
 			})
 
-			// var downloadControl = new DrawingBoard.Control.Download(board).addToBoard();
-			// var colorPicker = new DrawingBoard.Control.Color(board).addToBoard();
-			console.log('board',board)
-		
-			
-			console.log('boardimg',board.getImg())
-
 // drawingboard stuff \\
 
-    $scope.sendCanvi = () => {
-      const canvi = {
-      	Object:      	$scope.id,
-        draw: 				$scope.draw,
-        
-      }
+    $scope.sendCanvi = (canviId, index) => {
 
+			const img = board.getImg()
+			const imgInput = (board.blankCanvas === img) ? '' : img;
+      const canvi = {
+      	// Object:      	img,
+      	Object:      	$scope.id,
+        draw: 				imgInput,        
+      }
+      // console.log('id', canvi)
       $http
         .post('/api/canvis', canvi)
         .then(() =>  $scope.canvis.push(canvi))
         .catch(console.error)
+			// console.log('img', img)
     }
 
 	    $http
@@ -74,295 +95,15 @@ angular
 	        $scope.canvis = canvis
 	      )
 	   
-			$scope.remove = (canviId, index) => { 
-				$http
-				.post('/api/remove', {canviId})
-				.then(() => $scope.canvis.splice(index, 1))
-				console.log(canviId)    
-			}
+			// $scope.remove = (canviId, index) => { 
+			// 	$http
+			// 	.post('/api/remove', {canviId})
+			// 	.then(() => $scope.canvis.splice(index, 1))
+			// 	console.log('canviId',canviId)    
+			// }
 
+    console.log('board', board)
 
   })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// canvas stuff \\
-
-// var e = new function canvasLayer(location, id) {
-// let canvas = $('#canvas')
-// // let canvas = document.getElementById('#canvas')
-
-//     this.width = $(window).width();
-//     this.height = $(window).height();
-//     this.element = document.createElement('canvas');
-//     $(this.element)
-//        .attr('id', id)
-//        .text('unsupported browser')
-//        .width(this.width)
-//        .height(this.height)
-//        .appendTo(location);
-
-//     this.context = this.element.getContext("2d");
-// // }
-
-// // var context = canvas.get[0].getContext("2d");
-// // let context = canvas.getContext('2d')
-// let context = this.context//canvas.getContext('2d')
-// let radius = 5;
-// let dragging = false;
-// context.lineWidth = radius*2
-
-// let putPoint = (e)=>{
-// 	if(dragging){
-// 	context.lineTo(e.offsetX, e.offsetY)
-// 	context.stroke()
-// 	context.beginPath()
-// 	context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2)
-// 	context.fill()
-// 	context.beginPath()
-// 	context.moveTo(e.offsetX, e.offsetY)
-// 	}
-// }
-
-
-// let engage = (e)=>{
-// 	dragging = true
-// 	putPoint(e)
-// 	// console.log('e',e)
-// }
-
-// let disengage = ()=>{
-// 	dragging = false
-// 	context.beginPath()
-// }
-
-// $(engage).mousedown(function(e) {
-//   console.log('mousedown')
-//   // console.log('e',e)
-// // console.log('this', this)
-// });
-
-// $( putPoint).click(function(e) {
-//   $( "#canvas" ).mousemove();
-// });
-
-// $( disengage ).mouseup(function() {
-//   console.log('mouseup')
-// });
-
-
-// let testing = $('#canvas');
-// console.log("testing",testing);
-
-// $('#canvas').click(function() {
-//   console.log("I was clicked!");
-// });
-
-// }
-
-
-
-// };
-// canvasLayer()
-
-// window.onLoad()
-
-// $( "#canvas" ).bind({
-//   click: function() {
-//     dragging = true
-// 	putPoint(e)
-//   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// 'use strict'
-
-
-// angular
-// 	.module('drawingApp', ['ngRoute'])
-// 	.config($routeProvider =>
-// 		$routeProvider
-// 			.when('/', {
-// 				controller: 'MainCtrl',
-// 				templateUrl: 'partials/main.html'
-// 			})
-// 			.when('/list', {
-// 				controller: 'DrawCtrl',
-// 				templateUrl: 'partials/list.html'
-// 			})
-// 		)
-
-
-
-// 	.controller('MainCtrl', function ($scope, $http) {
-// 		$http
-// 			.get('/api/title')
-// 			.then(({data: {title}}) => {
-// 				$scope.title = title
-// 			})
-// 	})
-
-
-													
-//   .controller('DrawCtrl', function ($scope, $http) {
-//     $scope.sendList = () => {
-//       const list = {
-//       	Object:      $scope.id,
-//         job: 				 $scope.job,
-//         description: $scope.description,
-        
-//       }
-
-//       $http
-//         .post('/api/lists', list)
-//         .then(() =>  $scope.lists.push(list))
-//         .catch(console.error)
-//     }
-
-// 	    $http
-// 	      .get('/api/lists')
-// 	      .then(({ data: { lists }}) =>
-// 	        $scope.lists = lists
-// 	      )
-	   
-// 			$scope.remove = (listId, index) => { 
-// 				$http
-// 				.post('/api/remove', {listId})
-// 				.then(() => $scope.lists.splice(index, 1))
-// 				console.log(listId)    
-// 			}
-
-//   })
-// // canvas stuff
-// //  			canvas = document.getElementById('canvas')
-// // const context = canvas.getContext('2d')
-
-// // const radius = 5;
-// // const dragging = false;
-// // context.lineWidth = radius*2
-// // const putPoint = (e)=>{
-// // 	if(dragging){
-// // 	context.lineTo(e.offsetX, e.offsetY)
-// // 	context.stroke()
-// // 	context.beginPath()
-// // 	context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2)
-// // 	context.fill()
-// // 	context.beginPath()
-// // 	context.moveTo(e.offsetX, e.offsetY)
-// // 	}
-// // }
-
-// // const engage = (e)=>{
-// // 	dragging = true
-// // 	putPoint(e)
-// // }
-// // const disengage = ()=>{
-// // 	dragging = false
-// // 	context.beginPath()
-// // }
-
-// // canvas.addEventListener('mousedown', engage)
-// // canvas.addEventListener('mousemove', putPoint)
-// // canvas.addEventListener('mouseup', disengage)
-
-
-
+ 
 
